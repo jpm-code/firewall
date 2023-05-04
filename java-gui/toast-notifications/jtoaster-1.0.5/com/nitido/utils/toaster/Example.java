e81836f16650d7248cbaaba5d761b641a762ed2e
Jools Murphy <jools.murphy@protonmail.com>
2023-05-04 10:45:32 +0100

    Add toast notifications

---
+++ java-gui/toast-notifications/jtoaster-1.0.5/com/nitido/utils/toaster/Example.java
@@ -0,0 +1,15 @@
+package com.nitido.utils.toaster;
+
+public class Example
+{
+
+	/**
+	 * Simple Example...
+	 */
+	public static void main(String[] args)
+	{
+     Toaster toasterManager = new Toaster();
+     toasterManager.showToaster( "JToaster Hello World!" );
+	}
+
+}
